edb5c79fd53b3641e2e10a28a56b649302d570d4
Douae Abrouri <doabrour@c1r8p7.1337.ma>
2026-01-29 20:19:48 +0100

    ...

--- chunks.c
+++ chunks.c
@@ -6,7 +6,7 @@
 /*   By: doabrour <doabrour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 19:36:07 by doabrour          #+#    #+#             */
-/*   Updated: 2026/01/29 19:13:00 by doabrour         ###   ########.fr       */
+/*   Updated: 2026/01/29 20:04:21 by doabrour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,31 +56,46 @@ t_stack     *bee_or_not_to_bee(t_stack *b, int value)
 	return NULL;
 }
 
-int		how_much_pain_for_this_number(t_stack *b, int nb)
+int		how_much_pain_for_this_number(t_stack *b, t_stack *a, int nb)
 {
 	//step 1: find the position of the number and here i have a relation <cost_a = min(position, size_a - position)>
 	// i use cost_a = min(pos, size_a - pos) to found the smallest number of moves to bring a number to the top od stack_A;
 	// size_a - pos how many reverse rotations (rra) u need !
- 	t_stack *tmp;
+ 	t_stack *tmp_a;
 	int 	const_nb;
-	int		pos;
+	int		pos_a;
 	int		size_a;
 
-	tmp = b;
+	tmp_a = a;
 	const_nb = 0;
-	size_a = stack_size(b);
-	while(tmp != NULL)
+	size_a = stack_size(a);
+	while(tmp_a != NULL)
 	{
-		if(tmp->value != nb)
-			tmp = tmp->next;
-		else if(tmp->value == nb)
+		if(tmp_a->value != nb)
+			tmp_a = tmp_a->next;
+		else if(tmp_a->value == nb)
 		{
-			pos = tmp->index;
-			if (pos <= size_a / 2)
-				const_nb = pos;
+			pos_a = tmp_a->index;
+			if (pos_a <= size_a / 2)
+				const_nb = pos_a;
 			else
-				const_nb = size_a - pos;
+				const_nb = size_a - pos_a;
 		}
 	}
-	//step 2 : 
+	//step 2 : find the target node in stack B
+	int		size_b;
+	t_stack	*tmp_b;
+	int		pos_b;
+
+	tmp_b = bee_or_not_to_bee(b, nb);
+	size_b = stack_size(b);
+	pos_b = tmp_b->index;
+	if(pos_b <= size_b / 2)
+		const_nb = pos_b;
+	else
+		const_nb = size_b - pos_b;
+	//step 3: Decide rotation direction
+
+	
+	
 }
